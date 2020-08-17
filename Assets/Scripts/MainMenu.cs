5022d72dd55854cb2aaf7e35f6211c6f9e52b58e
lambSauce9 <45115684+lambSauce9@users.noreply.github.com>
2020-08-17 23:59:00 +0700

    Pushing code
    226 changed files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlaneTracking()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ImageTracking()
+    {
+        SceneManager.LoadScene(2);
+    }    
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void FaceTracking()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
